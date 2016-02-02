b4aef17e50ef6fcc7cec341d5ddafcae56e3b554
Christoffer Winterkvist <christoffer@hyper.no>
2016-02-02 15:52:22 +0100

    Start adding annotations and dummy text

--- SocialCoding/SocialCoding.swift
+++ SocialCoding/SocialCoding.swift
@@ -16,7 +16,13 @@ extension NSObject {
 
 class SocialCodingPlugin: NSObject {
 
-  lazy var textView: NSTextView =  {
+  lazy var clickGesture: NSClickGestureRecognizer = {
+    let gesture = NSClickGestureRecognizer()
+    gesture.action = "dismissComment:"
+    return gesture
+  }()
+
+  lazy var textView: NSTextView = { [unowned self] in
     let textView = NSTextView()
     textView.backgroundColor = NSColor(deviceRed: 254/255, green: 253/255, blue: 204/255, alpha: 1.0)
     return textView
@@ -44,6 +50,10 @@ class SocialCodingPlugin: NSObject {
       with: "zen_annotationAtSidebarPoint:")
 
     swizzle(aClass,
+      exchange: "sidebarWidth",
+      with: "zen_sidebarWidth")
+
+    swizzle(aClass,
       exchange: "_drawLineNumbersInSidebarRect:foldedIndexes:count:linesToInvert:linesToReplace:getParaRectBlock:",
       with: "zen_drawLineNumbersInSidebarRect:foldedIndexes:count:linesToInvert:linesToReplace:getParaRectBlock:")
   }
@@ -54,16 +64,41 @@ class SocialCodingPlugin: NSObject {
       class_getInstanceMethod(aClass, with)
     )
   }
+
+  func dismissComment(sender: AnyObject) {
+    NSLog("clicked it")
+    textView.removeFromSuperview()
+  }
+}
+
+extension SocialCodingPlugin: NSTextViewDelegate {
+
+  func textView(textView: NSTextView, clickedOnLink link: AnyObject, atIndex charIndex: Int) -> Bool {
+    NSLog("wee")
+    return true
+  }
 }
 
 extension NSRulerView {
 
   func drawBackground(color: NSColor, atLine: UInt) {
     var (a0, a1) = (CGRect(), CGRect())
-
     getParagraphRect(&a0, firstLineRect: &a1, forLineNumber: atLine)
+    a0.size.width = 2
     color.set()
     NSRectFill(a0)
+  }
+
+  func drawText(string: String, atLine: UInt) {
+    var (a0, a1) = (CGRect(), CGRect())
+    getParagraphRect(&a0, firstLineRect: &a1, forLineNumber: atLine)
+
+    let attributes = [NSFontAttributeName : lineNumberFont]
+    let attributedString = NSMutableAttributedString(string: string, attributes: attributes)
+    a0.origin.x += 1.0;
+    a0.origin.y += 2.0;
+    attributedString.addAttributes(["MyCustomTag" : true], range: NSRange(location: 0, length: attributedString.length))
+    attributedString.drawAtPoint(a0.origin)
   }
 
   func zen_annotationAtSidebarPoint(point: CGPoint) -> AnyObject? {
@@ -72,17 +107,20 @@ extension NSRulerView {
     if let plugin = plugin {
       if annotation == nil && point.x < CGFloat(sidebarWidth) {
         let line = lineNumberForPoint(point)
-        let attributedString = NSAttributedString(string: "foo")
+        let attributes = [NSFontAttributeName : lineNumberFont]
+        let attributedString = NSAttributedString(string: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis quis imperdiet nisl. Proin lobortis volutpat massa eu lacinia. Duis faucibus rhoncus efficitur. Donec iaculis lacus et erat eleifend luctus. Nulla ultrices eu magna non placerat. Sed quis eros vel urna tincidunt porttitor. Duis dignissim efficitur cursus.\n\nNunc at erat eget justo facilisis fermentum sit amet sit amet sapien. Vestibulum egestas mi at magna pulvinar commodo. Maecenas eget rutrum mi. Curabitur sollicitudin felis nisi, id pretium tortor eleifend nec. Suspendisse viverra quis diam a aliquam. Mauris in sem fringilla, sollicitudin risus luctus, consectetur diam. Ut a enim non sapien lobortis euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Duis nibh sapien, viverra id dapibus sed, fermentum eget felis. Phasellus tortor dui, euismod non est et, imperdiet consectetur sem. In hac habitasse platea dictumst. Donec ac ex at ex rhoncus interdum. Mauris magna sapien, aliquam finibus eros sed, pellentesque pulvinar turpis. Quisque eget ante lectus.\n\n Vestibulum consequat ac nisi in sollicitudin. Integer vel ornare ligula. Sed sollicitudin, metus vitae hendrerit facilisis, purus tellus vestibulum eros, id elementum dolor lacus vitae dolor. Suspendisse blandit condimentum lacus, non suscipit metus egestas sit amet. Nunc ac sodales velit. Fusce eget pharetra ante, vitae volutpat libero. Morbi vitae turpis id erat convallis tempus. Donec a lorem ut felis tempor semper. Aenean quam arcu, posuere vitae est non, convallis molestie purus. Curabitur gravida nunc eu feugiat aliquet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi convallis lobortis velit tempor finibus.", attributes: attributes)
         plugin.textView.textStorage?.setAttributedString(attributedString)
 
         var (a0, a1) = (CGRect(), CGRect())
         getParagraphRect(&a0, firstLineRect: &a1, forLineNumber: 60)
 
         plugin.textView.frame = NSRect(x: NSWidth(frame)+1.0, y: a0.origin.y, width: 500, height: 10)
+        plugin.clickGesture.target = plugin
+        plugin.textView.addGestureRecognizer(plugin.clickGesture)
 
         scrollView?.addSubview(plugin.textView)
       } else if plugin.textView.superview != nil {
-        plugin.textView.removeFromSuperview()
+        //plugin.textView.removeFromSuperview()
       }
     }
 
@@ -94,12 +132,14 @@ extension NSRulerView {
     lockFocus()
 
     drawBackground(NSColor(calibratedRed: 244/255, green: 222/255, blue: 202/255, alpha: 1.0), atLine: 60)
+    drawText("🚀", atLine: 60)
 
     unlockFocus()
 
     self.zen_drawLineNumbersInSidebarRect(rect, foldedIndexes: foldedIndexes, count: count, linesToInvert: linesToInvert, linesToReplace: linesToReplace, getParaRectBlock: getParaRectBlock)
   }
 
-  func zen_drawBackground(color: NSColor, atLine: Int) {
+  func zen_sidebarWidth() -> CGFloat {
+    return zen_sidebarWidth() + 2
   }
 }
