0683729420312ac1a11bd74eb5247a82ea2e0392
Christoffer Winterkvist <christoffer@hyper.no>
2016-02-03 10:43:52 +0100

    Sort code

--- SocialCoding/SocialCoding.swift
+++ SocialCoding/SocialCoding.swift
@@ -18,8 +18,8 @@ class SocialViewController: NSViewController {
 
   override func loadView() {
     let scrollView = NSScrollView(frame:CGRect(origin: CGPoint(x: 100, y: 100), size: CGSize(width: 1000, height: 1000)))
-    scrollView.hasVerticalScroller = true
     scrollView.autohidesScrollers = true
+    scrollView.hasVerticalScroller = true
 
     view = scrollView
     view.layer?.backgroundColor = NSColor.clearColor().CGColor
